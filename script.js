be4d1395367d5b4bf5345aefabe6b8fe98d09beb
Aditya Sajgotra <anukhajuria089@gmail.com>
2025-04-03 14:24:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-// inputField.addEventListener('keydown', function(event) {
-//     if (event.key === 'Enter') {
-//       alert('Enter key pressed!');
+document.addEventListener('DOMContentLoaded', () => {
+    const search = document.getElementsByClassName('search');
+
+    search[0].addEventListener('keydown', (e) => {
+          if (e.key ===  "Enter" && search[0].value == "Wheels"){
+            console.log("wheels are searched for!")
+          }
+    });
+});
