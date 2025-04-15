c0608bc4c19f61e49491152b81498dcb94b0e2d1
Aditya30307 <anukhajuria089@gmail.com>
2025-04-15 21:32:56 +0530

    fix page not found by addibng plugin

--- safe-wheels/vite.config.js
+++ safe-wheels/vite.config.js
@@ -1,5 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-    base: '/',
+  base: '/',
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'signup.html', // or adjust the path if it's in a subfolder like 'src/second.html'
+          dest: '.' // puts it at the root of dist
+        }
+      ]
+    })
+  ]
 })
